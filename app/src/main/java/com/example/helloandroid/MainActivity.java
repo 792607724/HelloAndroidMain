b8ac59fe9b4f2a5457d0adf7ef8e7306a533e582
CHENGUANGTAO <792607724@qq.com>
2022-01-24 10:22:23 +0800

    Update to java 8, 1.8, rebuild version by : sdkmanager --update sdkmanager --licenses

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.helloandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        Button btn2 = findViewById(R.id.button_2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "show", Toast.LENGTH_LONG).show();
+            }
+        });
+        btn2.setOnClickListener(new MyButton());
+    }
+
+    private class MyButton implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+            Toast.makeText(getApplicationContext(), "showoff", Toast.LENGTH_LONG).show();
+        }
     }
 }
