36604233b30a309c8036edf28b1ba5ab5294a0a2
cgt <792607724@qq.com>
2022-01-25 14:57:34 +0800

    Current Android Project based on Java 8 JDK1.8 and Android studio version:Android Studio Arctic Fox | 2020.3.1 Patch 4

--- app/src/main/java/com/example/helloandroid/MainActivity3.java
+++ app/src/main/java/com/example/helloandroid/MainActivity3.java
@@ -4,15 +4,18 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity3 extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity3 extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     private Button basicDialog, listDialog, singleChoiceDialog, multipleChoiceDialog;
+    private CheckBox checkbox_android, checkbox_ios, checkbox_web, checkbox_django, checkbox_python;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +35,22 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
         listDialog = findViewById(R.id.listDialog);
         singleChoiceDialog = findViewById(R.id.singleChoiceDialog);
         multipleChoiceDialog = findViewById(R.id.multipleChoiceDialog);
+        checkbox_android = findViewById(R.id.checkbox_android);
+        checkbox_ios = findViewById(R.id.checkbox_ios);
+        checkbox_web = findViewById(R.id.checkbox_web);
+        checkbox_django = findViewById(R.id.checkbox_django);
+        checkbox_python = findViewById(R.id.checkbox_python);
+
 
         basicDialog.setOnClickListener(this);
         listDialog.setOnClickListener(this);
         singleChoiceDialog.setOnClickListener(this);
         multipleChoiceDialog.setOnClickListener(this);
+        checkbox_android.setOnCheckedChangeListener(this);
+        checkbox_ios.setOnCheckedChangeListener(this);
+        checkbox_web.setOnCheckedChangeListener(this);
+        checkbox_django.setOnCheckedChangeListener(this);
+        checkbox_python.setOnCheckedChangeListener(this);
     }
 
 
@@ -58,6 +72,7 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
         }
     }
 
+
     private void basicDialogOperate() {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity3.this);
         builder.setIcon(R.drawable.laofuzi);
@@ -66,19 +81,19 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
         builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActivity3.this, "点击了确定", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity3.this, "点击了确定", Toast.LENGTH_SHORT).show();
             }
         });
         builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActivity3.this, "点击了取消", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity3.this, "点击了取消", Toast.LENGTH_SHORT).show();
             }
         });
         builder.setNeutralButton("好的", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActivity3.this, "点击了好的", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity3.this, "点击了好的", Toast.LENGTH_SHORT).show();
             }
         });
         AlertDialog alertDialog = builder.create();
@@ -94,7 +109,7 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActivity3.this, "点击了" + items[i], Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity3.this, "点击了" + items[i], Toast.LENGTH_SHORT).show();
             }
         });
         AlertDialog alertDialog = builder.create();
@@ -114,13 +129,13 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
         builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActivity3.this, "选择了" + items[index], Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity3.this, "选择了" + items[index], Toast.LENGTH_SHORT).show();
             }
         });
         builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActivity3.this, "你没有做出选择", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity3.this, "你没有做出选择", Toast.LENGTH_SHORT).show();
             }
         });
         AlertDialog alertDialog = builder.create();
@@ -147,11 +162,41 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
                         buffer.append(items[j] + " ");
                     }
                 }
-                Toast.makeText(MainActivity3.this, "选择了" + buffer.toString(), Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity3.this, "选择了" + buffer.toString(), Toast.LENGTH_SHORT).show();
             }
         });
         builder.setNegativeButton("取消", null);
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId()) {
+            case R.id.checkbox_android:
+                this.checkOperate("Android", b);
+                break;
+            case R.id.checkbox_web:
+                this.checkOperate("Web", b);
+                break;
+            case R.id.checkbox_django:
+                this.checkOperate("Django", b);
+                break;
+            case R.id.checkbox_ios:
+                this.checkOperate("IOS", b);
+                break;
+            case R.id.checkbox_python:
+                this.checkOperate("Python", b);
+                break;
+        }
+    }
+
+    private void checkOperate(String chosedItem, boolean b) {
+        if (b) {
+            Toast.makeText(MainActivity3.this, "你选择的是" + chosedItem, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity3.this, "你取消了选择" + chosedItem, Toast.LENGTH_SHORT).show();
+        }
     }
 }
