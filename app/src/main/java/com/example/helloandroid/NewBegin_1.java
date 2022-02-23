cf8a6d45d6a2b3ac7f34fac197667ef80500187f
CHENGUANGTAO <792607724@qq.com>
2022-02-23 16:09:11 +0800

    Update to java 8, 1.8, rebuild version by : sdkmanager --update sdkmanager --licenses

---
+++ app/src/main/java/com/example/helloandroid/NewBegin_1.java
@@ -0,0 +1,31 @@
+package com.example.helloandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewBegin_1 extends AppCompatActivity {
+
+    private Button btn_jumpToSDKTest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_begin1);
+
+        btn_jumpToSDKTest = findViewById(R.id.btn_jumpToSDKTest);
+        btn_jumpToSDKTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NewBegin_1.this, SDKTest_mainactivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+    }
+}
