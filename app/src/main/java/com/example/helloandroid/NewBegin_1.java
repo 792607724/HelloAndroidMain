de25e40cfcde27ca3d7b47a4c6099353db8a0b0b
cgt <792607724@qq.com>
2022-02-24 19:00:02 +0800

    Current Android Project based on Java 8 JDK1.8 and Android studio version:Android Studio Arctic Fox | 2020.3.1 Patch 4

--- app/src/main/java/com/example/helloandroid/NewBegin_1.java
+++ app/src/main/java/com/example/helloandroid/NewBegin_1.java
@@ -1,9 +1,6 @@
 package com.example.helloandroid;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +13,6 @@ public class NewBegin_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_begin2);
-
 
 //        btn_jumpToSDKTest = findViewById(R.id.btn_jumpToSDKTest);
 //        btn_jumpToSDKTest.setOnClickListener(new View.OnClickListener() {
