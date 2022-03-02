72971dbc47c038cd47be78401e5abb08537cb44f
CHENGUANGTAO <792607724@qq.com>
2022-03-02 10:19:25 +0800

    Update to java 8, 1.8, rebuild version by : sdkmanager --update sdkmanager --licenses

---
+++ app/src/main/java/com/example/helloandroid/NewBegin_2.java
@@ -0,0 +1,19 @@
+package com.example.helloandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewBegin_2 extends AppCompatActivity {
+
+    /**
+     * 单元4 Android的数据存储与数据共享程序设计
+     * @param savedInstanceState
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_begin3);
+    }
+}
